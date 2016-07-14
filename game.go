49f020b0e9750509aa48c8f39287284eebeefc0f
Michael Alexander <beefsack@gmail.com>
2016-07-14 16:44:30 +1000

    Font rendering

--- game.go
+++ game.go
@@ -1,13 +1,52 @@
 package acquire
 
 import (
+	"fmt"
 	"image"
 	"image/color"
+	"io/ioutil"
 
+	"github.com/golang/freetype/truetype"
 	"github.com/llgcode/draw2d"
 )
 
 type Game struct{}
+
+var fonts = []struct {
+	draw2d.FontData
+	path string
+}{
+	{
+		FontData: draw2d.FontData{
+			Name:   "Noto Sans",
+			Family: draw2d.FontFamilySans,
+			Style:  draw2d.FontStyleNormal,
+		},
+		path: "NotoSans-Regular.ttf",
+	},
+	{
+		FontData: draw2d.FontData{
+			Name:   "Noto Sans",
+			Family: draw2d.FontFamilySans,
+			Style:  draw2d.FontStyleNormal,
+		},
+		path: "NotoSans-Regular.ttf",
+	},
+}
+
+func init() {
+	for _, f := range fonts {
+		data, err := ioutil.ReadFile(f.path)
+		if err != nil {
+			panic(fmt.Sprintf("failed to read font '%s', %s", f.path, err))
+		}
+		font, err := truetype.Parse(data)
+		if err != nil {
+			panic(fmt.Sprintf("failed to parse font '%s', %s", f.path, err))
+		}
+		draw2d.RegisterFont(f.FontData, font)
+	}
+}
 
 func (g *Game) PlayerRender(
 	player int,
@@ -24,5 +63,14 @@ func (g *Game) PlayerRender(
 	gc.LineTo(10, float64(bounds.Max.Y-10))
 	gc.Close()
 	gc.FillStroke()
+	gc.SetFontData(draw2d.FontData{
+		Name:   "Noto Sans",
+		Family: draw2d.FontFamilySans,
+		Style:  draw2d.FontStyleNormal,
+	})
+	gc.SetFillColor(color.RGBA{0, 0, 0, 255})
+	gc.SetFontSize(20)
+	gc.Rotate(0.1)
+	gc.FillStringAt("BLAH!", 50, 50)
 	return nil
 }
